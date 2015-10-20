4d69abda83e2e5e9c306e83182c14b0ddc2b7448
Flavio <perezflavio@gmail.com>
2015-10-20 18:15:52 +0200

    when I dropped a feature to merge work with Damien. You should NOT work on master branch you fool

--- webapp/static/js/colibri_poses.js
+++ webapp/static/js/colibri_poses.js
@@ -61,6 +61,34 @@ function colibri_update_properties(selection){
 // get a cookie by his name (k)
 function get_cookie(k){return(document.cookie.match('(^|; )'+k+'=([^;]*)')||0)[2]}
 
+var QueryString = function () {
+  // This function is anonymous, is executed immediately and 
+  // the return value is assigned to QueryString!
+  var query_string = {};
+  var query = window.location.search.substring(1);
+  var vars = query.split("&");
+  for (var i=0;i<vars.length;i++) {
+    var pair = vars[i].split("=");
+        // If first entry with this name
+    if (typeof query_string[pair[0]] === "undefined") {
+      query_string[pair[0]] = decodeURIComponent(pair[1]);
+        // If second entry with this name
+    } else if (typeof query_string[pair[0]] === "string") {
+      var arr = [ query_string[pair[0]],decodeURIComponent(pair[1]) ];
+      query_string[pair[0]] = arr;
+        // If third or later entry with this name
+    } else {
+      query_string[pair[0]].push(decodeURIComponent(pair[1]));
+    }
+  } 
+    return query_string;
+}();
+
+
+function attach_events(){
+
+
+}
 
 // last action hero part
 $( document ).ready(function() {
@@ -95,6 +123,11 @@ $( document ).ready(function() {
         source_mouseX = 0,
         merge_factor = 0,
         previous_merge_factor = -1;
+
+
+    if (QueryString.selectedPose){
+        colibri_update_properties($("#pose_" + QueryString.selectedPose));
+    }
 
     // actions on thumbnails
 
@@ -214,6 +247,14 @@ $( document ).ready(function() {
             $('#mousetooltip').hide();
             // call here final pose ?
         }
+    });
+
+    // New pose
+
+    $(".new_pose")
+    .on("click", function(e){
+        alert(window.location);
+        window.location.replace(".?newPose");
     });
 
 
